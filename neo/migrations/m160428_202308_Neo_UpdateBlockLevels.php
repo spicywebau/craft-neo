871811ee68d4d930ac53b493c4965e64599e32dc
Benjamin Fleming <benjfleming21@gmail.com>
2016-05-15 11:06:59 +1000

    Removed dependency on record in old migration

--- neo/migrations/m160428_202308_Neo_UpdateBlockLevels.php
+++ neo/migrations/m160428_202308_Neo_UpdateBlockLevels.php
@@ -5,8 +5,7 @@ class m160428_202308_Neo_UpdateBlockLevels extends BaseMigration
 {
 	public function safeUp()
 	{
-		$tableName = (new Neo_BlockRecord())->getTableName();
-		craft()->db->createCommand()->update($tableName, ['level' => new \CDbExpression('level + 1')]);
+		craft()->db->createCommand()->update('neoblocks', ['level' => new \CDbExpression('level + 1')]);
 
 		return true;
 	}
