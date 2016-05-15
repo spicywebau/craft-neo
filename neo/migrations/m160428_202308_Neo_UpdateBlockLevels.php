5451d618a4b4ea5326b8f53600504cc171647b6d
Benjamin Fleming <benjfleming21@gmail.com>
2016-05-15 11:17:24 +1000

    Use migration logging

--- neo/migrations/m160428_202308_Neo_UpdateBlockLevels.php
+++ neo/migrations/m160428_202308_Neo_UpdateBlockLevels.php
@@ -5,7 +5,7 @@ class m160428_202308_Neo_UpdateBlockLevels extends BaseMigration
 {
 	public function safeUp()
 	{
-		craft()->db->createCommand()->update('neoblocks', ['level' => new \CDbExpression('level + 1')]);
+		$this->update('neoblocks', ['level' => new \CDbExpression('level + 1')]);
 
 		return true;
 	}
--- neo/migrations/m160515_005002_Neo_UpdateBlockStructure.php
+++ neo/migrations/m160515_005002_Neo_UpdateBlockStructure.php
@@ -6,21 +6,27 @@ class m160515_005002_Neo_UpdateBlockStructure extends BaseMigration
 	public function safeUp()
 	{
 		// Create the craft_neoblockstructures table
-		craft()->db->createCommand()->createTable('neoblockstructures', [
+		$this->createTable('neoblockstructures', [
 			'structureId' => ['column' => 'integer', 'required' => true],
 			'ownerId'     => ['column' => 'integer', 'required' => true],
 			'fieldId'     => ['column' => 'integer', 'required' => true],
 		], null, true);
 
 		// Add indexes to craft_neoblockstructures
-		craft()->db->createCommand()->createIndex('neoblockstructures', 'structureId', false);
-		craft()->db->createCommand()->createIndex('neoblockstructures', 'ownerId', false);
-		craft()->db->createCommand()->createIndex('neoblockstructures', 'fieldId', false);
+		$this->createIndex('neoblockstructures', 'structureId', false);
+		$this->createIndex('neoblockstructures', 'ownerId', false);
+		$this->createIndex('neoblockstructures', 'fieldId', false);
 
 		// Add foreign keys to craft_neoblockstructures
-		craft()->db->createCommand()->addForeignKey('neoblockstructures', 'structureId', 'structures', 'id', 'CASCADE', null);
-		craft()->db->createCommand()->addForeignKey('neoblockstructures', 'ownerId', 'elements', 'id', 'CASCADE', null);
-		craft()->db->createCommand()->addForeignKey('neoblockstructures', 'fieldId', 'fields', 'id', 'CASCADE', null);
+		$this->addForeignKey('neoblockstructures', 'structureId', 'structures', 'id', 'CASCADE', null);
+		$this->addForeignKey('neoblockstructures', 'ownerId', 'elements', 'id', 'CASCADE', null);
+		$this->addForeignKey('neoblockstructures', 'fieldId', 'fields', 'id', 'CASCADE', null);
+
+		// Migrate existing fields
+		//$this->select('neoblocks');
+
+		$this->dropColumn('neoblocks', 'sortOrder');
+		$this->dropColumn('neoblocks', 'level');
 
 		return true;
 	}
