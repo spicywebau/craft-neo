63c001b9d3b7185466c088008cf93e20f4844724
Benjamin Fleming <benjfleming21@gmail.com>
2016-05-15 16:35:58 +1000

    Completed block structure migration

--- neo/migrations/m160515_005002_Neo_UpdateBlockStructure.php
+++ neo/migrations/m160515_005002_Neo_UpdateBlockStructure.php
@@ -23,11 +23,85 @@ class m160515_005002_Neo_UpdateBlockStructure extends BaseMigration
 		$this->addForeignKey('neoblockstructures', 'fieldId', 'fields', 'id', 'CASCADE', null);
 
 		// Migrate existing fields
-		//$this->select('neoblocks');
+		$structures = [];
+		$parentStacks = [];
+		$rawBlocks = craft()->db->createCommand()
+			->select('*')
+			->from('neoblocks')
+			->order('sortOrder asc')
+			->queryAll();
 
+		if($rawBlocks)
+		{
+			foreach($rawBlocks as $rawBlock)
+			{
+				$block = new Neo_BlockModel_1_0_2();
+				$block->id = intval($rawBlock['id']);
+				$block->ownerId = intval($rawBlock['ownerId']);
+				$block->fieldId = intval($rawBlock['fieldId']);
+				$block->typeId = intval($rawBlock['typeId']);
+				$block->level = intval($rawBlock['level']);
+
+				$key = $block->ownerId . ':' . $block->fieldId;
+
+				if(!isset($structures[$key]))
+				{
+					$structures[$key] = new StructureModel();
+					$parentStacks[$key] = [];
+
+					craft()->structures->saveStructure($structures[$key]);
+
+					$this->insert('neoblockstructures', [
+						'structureId' => $structures[$key]->id,
+						'ownerId' => $block->ownerId,
+						'fieldId' => $block->fieldId,
+					]);
+				}
+
+				$structure = $structures[$key];
+				$parentStack = $parentStacks[$key];
+
+				while(!empty($parentStack) && $block->level <= $parentStack[count($parentStack) - 1]->level)
+				{
+					array_pop($parentStack);
+				}
+
+				if(empty($parentStack))
+				{
+					craft()->structures->appendToRoot($structure->id, $block);
+				}
+				else
+				{
+					$parentBlock = $parentStack[count($parentStack) - 1];
+					craft()->structures->append($structure->id, $block, $parentBlock);
+				}
+
+				array_push($parentStack, $block);
+
+				$structures[$key] = $structure;
+				$parentStacks[$key] = $parentStack;
+			}
+		}
+
+		// Remove columns from craft_neoblocks
 		$this->dropColumn('neoblocks', 'sortOrder');
 		$this->dropColumn('neoblocks', 'level');
 
 		return true;
+	}
+}
+
+class Neo_BlockModel_1_0_2 extends BaseElementModel
+{
+	protected $elementType = 'Neo_Block';
+
+	protected function defineAttributes()
+	{
+		return array_merge(parent::defineAttributes(), [
+			'fieldId' => AttributeType::Number,
+			'ownerId' => AttributeType::Number,
+			'typeId' => AttributeType::Number,
+			'level' => AttributeType::Number,
+		]);
 	}
 }
