af54da02d1549db8a5c360650b482a8b614c6a4a
Benjamin Fleming <benjfleming21@gmail.com>
2016-06-10 12:09:47 +1000

    Fixed Reasons 1.0.4 support #24

--- src/plugins/reasons/Renderer.js
+++ src/plugins/reasons/Renderer.js
@@ -1,3 +1,5 @@
+import $ from 'jquery'
+
 export default Renderer => class extends Renderer
 {
 	addEventListeners()
@@ -16,4 +18,59 @@ export default Renderer => class extends Renderer
 
 	addLivePreviewListeners() {}
 	removeLivePreviewListeners() {}
+
+	initToggleFields()
+	{
+		// Get all current fields
+		this.$fields = $(this.getFieldsSelector())
+
+		if(this.$fields.length === 0)
+		{
+			return false
+		}
+
+		// Get toggle field IDs
+		const toggleFieldIds = []
+		for(let fieldId in this.conditionals)
+		{
+			for(let i = 0; i < this.conditionals[fieldId].length; i++)
+			{
+				toggleFieldIds.push(this.conditionals[fieldId][i][0].fieldId)
+			}
+		}
+
+		// Loop over fields and add data-id attribute
+		const self = this
+		this.$fields.each(function()
+		{
+			const $field = $(this)
+
+			if($field.attr('id') === undefined)
+			{
+				return
+			}
+
+			const fieldHandle = $field.attr('id').split('-').slice(-2, -1)[0] || false
+			const fieldId = Craft.ReasonsPlugin.getFieldIdByHandle(fieldHandle)
+
+			if(fieldId)
+			{
+				$field.attr('data-id', fieldId)
+			}
+
+			// Is this a target field?
+			if(self.conditionals[fieldId])
+			{
+				$field.attr('data-target', 1)
+			}
+
+			// Is this a toggle field
+			if(toggleFieldIds.indexOf(parseInt(fieldId)) > -1)
+			{
+				$field.attr('data-toggle', 1)
+			}
+		})
+
+		return true
+	}
 }
