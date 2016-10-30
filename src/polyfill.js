b30839b2f133cf4c6b25a4359444b7369b8080e7
Benjamin Fleming <benjfleming21@gmail.com>
2016-10-31 09:51:37 +1100

    Uhh, I confused Twig and JS comment styles...

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,8 +1,8 @@
 /**
  * ES2015 Javascript polyfill
- # The Javascript in this plugin is written using the latest features in the language. Not all browsers support the
- # features used, so the following code loads a polyfill provided some random new features exist. This polyfill is
- # hundreds of kilobytes in size, so it's important to only load it if absolutely necessary.
+ * The Javascript in this plugin is written using the latest features in the language. Not all browsers support the
+ * features used, so the following code loads a polyfill provided some random new features exist. This polyfill is
+ * hundreds of kilobytes in size, so it's important to only load it if absolutely necessary.
  */
 
 if(!Array.from || !Object.assign || typeof Symbol == 'undefined')
