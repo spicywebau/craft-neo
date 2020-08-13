1c98a1007703e5ff2b0787c26b00fb29410a7b5c
Thomas Templeton <thomas.templeton.dev@gmail.com>
2020-08-14 00:30:28 +1000

    Convert Craft 2->3 migration to match recommended Craft code style

--- src/migrations/m181022_123749_craft3_upgrade.php
+++ src/migrations/m181022_123749_craft3_upgrade.php
@@ -1,13 +1,13 @@
 <?php
+
 namespace benf\neo\migrations;
 
+use benf\neo\Field;
+use benf\neo\elements\Block;
 use Craft;
 use craft\db\Migration;
 use craft\db\Query;
 use craft\helpers\MigrationHelper;
-
-use benf\neo\Field;
-use benf\neo\elements\Block;
 
 /**
  * m181022_123749_craft3_upgrade migration.
@@ -18,67 +18,57 @@ use benf\neo\elements\Block;
  */
 class m181022_123749_craft3_upgrade extends Migration
 {
-	/**
-	 * @inheritdoc
-	 */
-	public function safeUp()
-	{
-		// Due to an issue with Neo 2 installations from pre-release trying to run this migration on update to Neo 2.2,
-		// we need to ensure that this is actually an upgrade from the latest release of Neo 1.  Easy way to do this is
-		// to check whether the `neoblocks` table has a `collapsed` column.  If it doesn't, we have nothing to do here.
-		$dbService = Craft::$app->getDb();
-		$hasCollapsed = $dbService->getSchema()->getTableSchema('{{%neoblocks}}')->getColumn('collapsed') !== null;
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        // Due to an issue with Neo 2 installations from pre-release trying to run this migration on update to Neo 2.2,
+        // we need to ensure that this is actually an upgrade from the latest release of Neo 1.  Easy way to do this is
+        // to check whether the `neoblocks` table has a `collapsed` column.  If it doesn't, we have nothing to do here.
+        $dbService = Craft::$app->getDb();
+        $hasCollapsed = $dbService->getSchema()->getTableSchema('{{%neoblocks}}')->getColumn('collapsed') !== null;
 
-		if (!$hasCollapsed)
-		{
-			return;
-		}
+        if (!$hasCollapsed) {
+            return;
+        }
 
-		// Now, proceed with the upgrade
-		$cacheService = Craft::$app->getCache();
+        // Now, proceed with the upgrade
+        $this->update('{{%elements}}', ['type' => Block::class], ['type' => 'Neo_Block']);
+        $this->update('{{%fields}}', ['type' => Field::class], ['type' => 'Neo']);
 
-		$this->update('{{%elements}}', [
-			'type' => Block::class
-		], ['type' => 'Neo_Block']);
+        // Move the `neoblocks` table's `collapsed` column data to the cache and then drop the column
+        $blocks = (new Query())
+            ->select(['id', 'collapsed'])
+            ->from(['{{%neoblocks}}'])
+            ->all();
 
-		$this->update('{{%fields}}', [
-			'type' => Field::class
-		], ['type' => 'Neo']);
+        foreach ($blocks as $block) {
+            $cacheKey = "neoblock-" . $block['id'] . "-collapsed";
 
-		// Move the `neoblocks` table's `collapsed` column data to the cache and then drop the column
-		$blocks = (new Query())
-			->select(['id', 'collapsed'])
-			->from(['{{%neoblocks}}'])
-			->all();
+            if ($block['collapsed']) {
+                Craft::$app->getCache()->add($cacheKey, 1);
+            }
+        }
 
-		foreach ($blocks as $block)
-		{
-			$cacheKey = "neoblock-" . $block['id'] . "-collapsed";
+        $this->dropColumn('{{%neoblocks}}', 'collapsed');
 
-			if ($block['collapsed'])
-			{
-				$cacheService->add($cacheKey, 1);
-			}
-		}
+        // Rename `ownerLocale__siteId` columns to `ownerSiteId` and drop old `ownerLocale` columns
+        MigrationHelper::renameColumn('{{%neoblocks}}', 'ownerLocale__siteId', 'ownerSiteId', $this);
+        $this->dropColumn('{{%neoblocks}}', 'ownerLocale');
+        MigrationHelper::renameColumn('{{%neoblockstructures}}', 'ownerLocale__siteId', 'ownerSiteId', $this);
+        $this->dropColumn('{{%neoblockstructures}}', 'ownerLocale');
 
-		$this->dropColumn('{{%neoblocks}}', 'collapsed');
+        // Rename `neogroups` table to `neoblocktypegroups`
+        MigrationHelper::renameTable('{{%neogroups}}', '{{%neoblocktypegroups}}', $this);
+    }
 
-		// Rename `ownerLocale__siteId` columns to `ownerSiteId` and drop old `ownerLocale` columns
-		MigrationHelper::renameColumn('{{%neoblocks}}', 'ownerLocale__siteId', 'ownerSiteId', $this);
-		$this->dropColumn('{{%neoblocks}}', 'ownerLocale');
-		MigrationHelper::renameColumn('{{%neoblockstructures}}', 'ownerLocale__siteId', 'ownerSiteId', $this);
-		$this->dropColumn('{{%neoblockstructures}}', 'ownerLocale');
-
-		// Rename `neogroups` table to `neoblocktypegroups`
-		MigrationHelper::renameTable('{{%neogroups}}', '{{%neoblocktypegroups}}', $this);
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	public function safeDown()
-	{
-		echo "m181022_123749_craft3_upgrade cannot be reverted.\n";
-		return false;
-	}
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m181022_123749_craft3_upgrade cannot be reverted.\n";
+        return false;
+    }
 }
