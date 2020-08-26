32d2a0831ec0d2c9470abc3885ede1a934c934b4
Thomas Templeton <thomas.templeton.dev@gmail.com>
2020-08-26 22:12:52 +1000

    Convert src/gql/resolvers/elements/Block.php to indent with spaces, not tabs

--- src/gql/resolvers/elements/Block.php
+++ src/gql/resolvers/elements/Block.php
@@ -14,45 +14,45 @@ use craft\gql\base\ElementResolver;
  */
 class Block extends ElementResolver
 {
-	/**
-	 * @inheritdoc
-	 */
-	public static function prepareQuery($source, array $arguments, $fieldName = null)
-	{
-		// If this is the beginning of a resolver chain, start fresh
-		if ($source === null) {
-			// get the first level
-			$query = BlockElement::find()->level(1);
-			
-			// If not, get the prepared element query
-		} else {
-			$query = $source->$fieldName;
-		}
-		
-		// If it's preloaded, it's preloaded.
-		if (is_array($query)) {
-			
-			// if it's preloaded, return the first level of the neo field only (child elements will be retrieved using `children`.
-			$newQuery = [];
-			
-			foreach ($query as $q) {
-				if ((int)$q->level === 1) {
-					$newQuery[] = $q;
-				}
-			}
-			
-			// if any level 1 blocks
-			if (count($newQuery)) {
-				return $newQuery;
-			}
-			
-			return $query;
-		}
-		
-		foreach ($arguments as $key => $value) {
-			$query->$key($value);
-		}
-		
-		return $query;
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function prepareQuery($source, array $arguments, $fieldName = null)
+    {
+        // If this is the beginning of a resolver chain, start fresh
+        if ($source === null) {
+            // get the first level
+            $query = BlockElement::find()->level(1);
+
+            // If not, get the prepared element query
+        } else {
+            $query = $source->$fieldName;
+        }
+
+        // If it's preloaded, it's preloaded.
+        if (is_array($query)) {
+
+            // if it's preloaded, return the first level of the neo field only (child elements will be retrieved using `children`.
+            $newQuery = [];
+
+            foreach ($query as $q) {
+                if ((int)$q->level === 1) {
+                    $newQuery[] = $q;
+                }
+            }
+
+            // if any level 1 blocks
+            if (count($newQuery)) {
+                return $newQuery;
+            }
+
+            return $query;
+        }
+
+        foreach ($arguments as $key => $value) {
+            $query->$key($value);
+        }
+
+        return $query;
+    }
 }
