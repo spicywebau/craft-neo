b90a11a9e58ff96e7f806f8d12f8176d82cf480a
Thomas Templeton <thomas.templeton.dev@gmail.com>
2021-05-30 22:25:07 +1000

    Convert src/gql/types/element/Block.php indentation to spaces

--- src/gql/types/elements/Block.php
+++ src/gql/types/elements/Block.php
@@ -18,62 +18,62 @@ use GraphQL\Type\Definition\ResolveInfo;
  */
 class Block extends ObjectType
 {
-	/**
-	 * @inheritdoc
-	 */
-	public function __construct(array $config)
-	{
-		$config['interfaces'] = [
-			NeoBlockInterface::getType(),
-			ElementInterface::getType()
-		];
-		
-		parent::__construct($config);
-	}
-	
-	/**
-	 * @inheritdoc
-	 */
-	protected function resolve($source, $arguments, $context, ResolveInfo $resolveInfo)
-	{
-		$fieldName = $resolveInfo->fieldName;
-		
-		if ($fieldName === 'typeHandle') {
-			return $source->getType()->handle;
-		}
-		
-		if ($fieldName === 'children') {
-			
-			$newBlocks = [];
-			$blocks = $source->$fieldName;
-			$sourceLevel = (int)$source->level + 1;
-			
-			// blocks array cannot be trusted. it will most likely be out of order and cached.
-			// we should retrieve the children blocks by query instead so it'll always be in the correct order.
-			
-			// -- old comment --
-			// because of how the children is retrieve the blocks are located in the parent,
-			// which is why we now have to retrieve them by query
-			// if there's none return the default.
-			// -- old comment --
-			$children = $source->getDescendants()->level($sourceLevel)->all();
-			
-			if(count($children) and is_array($children)) {
-				
-				foreach ($children as $block) {
-					if ((int)$block->level === $sourceLevel) {
-						$newBlocks[] = $block;
-					}
-				}
-			}
-			
-			if (count($newBlocks)) {
-				return $newBlocks;
-			}
-			
-		}
-		
-		return $source->$fieldName;
-	}
-	
+    /**
+     * @inheritdoc
+     */
+    public function __construct(array $config)
+    {
+        $config['interfaces'] = [
+            NeoBlockInterface::getType(),
+            ElementInterface::getType()
+        ];
+        
+        parent::__construct($config);
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    protected function resolve($source, $arguments, $context, ResolveInfo $resolveInfo)
+    {
+        $fieldName = $resolveInfo->fieldName;
+        
+        if ($fieldName === 'typeHandle') {
+            return $source->getType()->handle;
+        }
+        
+        if ($fieldName === 'children') {
+            
+            $newBlocks = [];
+            $blocks = $source->$fieldName;
+            $sourceLevel = (int)$source->level + 1;
+            
+            // blocks array cannot be trusted. it will most likely be out of order and cached.
+            // we should retrieve the children blocks by query instead so it'll always be in the correct order.
+            
+            // -- old comment --
+            // because of how the children is retrieve the blocks are located in the parent,
+            // which is why we now have to retrieve them by query
+            // if there's none return the default.
+            // -- old comment --
+            $children = $source->getDescendants()->level($sourceLevel)->all();
+            
+            if(count($children) and is_array($children)) {
+                
+                foreach ($children as $block) {
+                    if ((int)$block->level === $sourceLevel) {
+                        $newBlocks[] = $block;
+                    }
+                }
+            }
+            
+            if (count($newBlocks)) {
+                return $newBlocks;
+            }
+            
+        }
+        
+        return $source->$fieldName;
+    }
+    
 }
