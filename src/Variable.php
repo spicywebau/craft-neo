8f0908dc4d0b1ccfc39d090b11e36730a638bc71
Thomas Templeton <thomas.templeton.dev@gmail.com>
2021-12-19 21:40:21 +1100

    Convert Variable indentation to spaces

--- src/Variable.php
+++ src/Variable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace benf\neo;
 
 use benf\neo\elements\Block;
@@ -14,28 +15,28 @@ use benf\neo\elements\db\BlockQuery;
  */
 class Variable
 {
-	/**
-	 * Get Neo blocks on their own, without requiring an owner element.
-	 *
-	 * If possible, avoid using this function. Neo blocks are supposed to be tied explicitly to elements, and the use of
-	 * this function ignores this relationship. Using this function can open you up to all kinds of performance and
-	 * unexpected behavioural issues if you're not careful.
-	 *
-	 * @param array|null $criteria
-	 * @return BlockQuery
-	 */
-	public function blocks(array $criteria = null): BlockQuery
-	{
-		$query = Block::find();
+    /**
+     * Get Neo blocks on their own, without requiring an owner element.
+     *
+     * If possible, avoid using this function. Neo blocks are supposed to be tied explicitly to elements, and the use of
+     * this function ignores this relationship. Using this function can open you up to all kinds of performance and
+     * unexpected behavioural issues if you're not careful.
+     *
+     * @param array|null $criteria
+     * @return BlockQuery
+     */
+    public function blocks(array $criteria = null): BlockQuery
+    {
+        $query = Block::find();
 
-		if ($criteria !== null)
-		{
-			foreach ($criteria as $param => $value)
-			{
-				$query->$param = $value;
-			}
-		}
+        if ($criteria !== null)
+        {
+            foreach ($criteria as $param => $value)
+            {
+                $query->$param = $value;
+            }
+        }
 
-		return $query;
-	}
+        return $query;
+    }
 }
