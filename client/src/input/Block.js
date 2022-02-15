a6d67572686d9e6dfebce50e1c7b4b3cf9f30c3c
brandonkelly <brandon@pixelandtonic.com>
2022-02-15 08:46:00 -0800

    Fixed #541

--- client/src/input/Block.js
+++ client/src/input/Block.js
@@ -958,7 +958,7 @@ export default Garnish.Base.extend({
     $('.lightswitch', this.$contentContainer).lightswitch()
     $('.nicetext', this.$contentContainer).nicetext()
     $('.formsubmit', this.$contentContainer).formsubmit()
-    $('.menubtn', this.$contentContainer).menubtn()
+    $('.menubtn:not([data-disclosure-trigger])', this.$contentContainer).menubtn()
     $('.datetimewrapper', this.$contentContainer).datetime()
   },
 
