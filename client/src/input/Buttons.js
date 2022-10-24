b2ab2f74b7b3d2c17bf6e8d279370e35fd0d7732
Thomas Templeton <thomas.templeton.dev@gmail.com>
2022-10-24 22:52:53 +1100

    Hide button container if no buttons are rendered

--- client/src/input/Buttons.js
+++ client/src/input/Buttons.js
@@ -181,6 +181,19 @@ export default Garnish.Base.extend({
   initUi () {
     $('.menubtn', this.$container).menubtn()
     this.updateResponsiveness()
+
+    // If no buttons were rendered (e.g. if all valid block types are disabled for the user), hide the button container
+    if (this.$buttonsContainer.children().length === 0) {
+      const parent = this.$container.parent()
+      const grandParent = parent.parent()
+      const childrenContainer = grandParent.children('.ni_blocks')
+
+      if (childrenContainer.length === 0 || childrenContainer.children().length === 0) {
+        grandParent.addClass('hidden')
+      } else {
+        parent.addClass('hidden')
+      }
+    }
   },
 
   getBlockTypes () {
