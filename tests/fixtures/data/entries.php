afd740f91bd194b5bb82772a77e3e72e78d4c1b0
Thomas Templeton <thomas.templeton.dev@gmail.com>
2022-12-14 12:30:21 +1100

    Add test project config and entries fixture

---
+++ tests/fixtures/data/entries.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    [
+        'id' => null,
+        'sectionId' => '1',
+        'typeId' => '1',
+        'authorId' => '1',
+        'uid' => 'entry-000000000000000000000000000001',
+        'title' => 'Test Entry 1',
+        'slug' => 'testEntry1',
+    ],
+];
