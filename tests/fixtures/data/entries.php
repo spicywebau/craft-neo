a6d65af8ebf4704f50e7f898b2cb5527017d4869
Thomas Templeton <thomas.templeton.dev@gmail.com>
2023-02-10 22:22:39 +1100

    Add test for #682

--- tests/fixtures/data/entries.php
+++ tests/fixtures/data/entries.php
@@ -78,4 +78,31 @@ return [
             ],
         ],
     ],
+    [
+        'id' => null,
+        'sectionId' => '1',
+        'typeId' => '1',
+        'authorId' => '1',
+        'uid' => 'entry-000000000000000000000000000002',
+        'title' => 'Block query for ID gets only live block test',
+        'slug' => 'block-query-for-id-gets-only-live-block-test',
+        'neoField1' => [
+            'new1' => [
+                'level' => 1,
+                'type' => 'plainText',
+                'enabled' => true,
+                'fields' => [
+                    'plainTextField' => '1',
+                ],
+            ],
+            'new1' => [
+                'level' => 2,
+                'type' => 'plainText',
+                'enabled' => true,
+                'fields' => [
+                    'plainTextField' => '2',
+                ],
+            ],
+        ]
+    ]
 ];
---
+++ tests/unit/BlockQueryUnitTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace spicyweb\neotests\unit;
+
+use benf\neo\elements\Block;
+use Craft;
+use craft\elements\Entry;
+use craft\test\TestCase;
+use spicyweb\neotests\fixtures\EntriesFixture;
+use UnitTester;
+
+/**
+ * Class BlockQueryUnitTest
+ *
+ * @package spicyweb\neotests\unit
+ * @author Spicy Web <plugins@spicyweb.com.au>
+ */
+class BlockQueryUnitTest extends TestCase
+{
+    /**
+     * @var UnitTester
+     */
+    protected UnitTester $tester;
+
+    /**
+     * @inheritdoc
+     */
+    public function _fixtures(): array
+    {
+        return [
+            'entries' => EntriesFixture::class,
+        ];
+    }
+
+    /**
+     * Tests that block queries for a block ID return only the live block, and not any other data for that block on a
+     * draft's block structure.
+     */
+    public function testIdGetsLiveBlock(): void
+    {
+        $entry = $this->_entry();
+        $firstBlockId = $entry->getFieldValue('neoField1')->one()->id;
+        $draft = Craft::$app->getDrafts()->createDraft($entry);
+        $shouldHaveOneBlock = Block::find()
+            ->id($firstBlockId)
+            ->all();
+        $this->assertSame(
+            1,
+            count($shouldHaveOneBlock)
+        );
+    }
+
+    /**
+     * Gets the entry to use for block children tests.
+     *
+     * @return Entry
+     */
+    private function _entry(): Entry
+    {
+        return Craft::$app->getElements()->getElementByUid('entry-000000000000000000000000000002', Entry::class);
+    }
+}
