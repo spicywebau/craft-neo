620c0bd0e126f593ffca5cb5e797730f92624b65
Thomas Templeton <thomas.templeton.dev@gmail.com>
2025-02-20 16:03:57 +1100

    Composer update and fix tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -12,6 +12,7 @@ define('CRAFT_CONFIG_PATH', __DIR__ . '/_craft/config');
 define('CRAFT_MIGRATIONS_PATH', __DIR__ . '/_craft/migrations');
 define('CRAFT_TRANSLATIONS_PATH', __DIR__ . '/_craft/translations');
 define('CRAFT_VENDOR_PATH', dirname(__DIR__) . '/vendor');
+define('CRAFT_ROOT_PATH', dirname(__DIR__));
 
 $devMode = true;
 
